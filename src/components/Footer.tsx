1c1c4ceb56de69d299e9c1a3816d0196882e0b1e
chlorineCtrl <abirhasan5@iut-dhaka.edu>
2023-03-18 11:42:52 +0600

    Added Navbar and Footer Components

---
+++ src/components/Footer.tsx
@@ -0,0 +1,7 @@
+import { NextComponentType } from "next";
+
+const Footer: NextComponentType = () => {
+  return <div>this is footer</div>;
+};
+
+export default Footer;
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,14 @@
+import { NextComponentType } from "next";
+
+const Navbar: NextComponentType = () => {
+  return (
+    <div>
+      <div>
+        <h1 className="font-myfont1">AllGood</h1>
+      </div>
+      <div></div>
+    </div>
+  );
+};
+
+export default Navbar;
