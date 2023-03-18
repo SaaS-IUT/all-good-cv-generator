fa0ad5bb89058d1438d6c73015d6de2bb7d6b53f
Sajid Chowdhury <sajidahmed@iut-dhaka.edu>
2023-03-18 21:23:43 +0600

    merge conflict resolved, skills

--- src/pages/generating.tsx
+++ src/pages/generating.tsx
@@ -10,6 +10,9 @@ const generating: NextPage = () => {
     <div>
         <div className="flex">
             <Enterinfo />
+            <button  type="submit" className="mx-5 mt-5  bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded">
+              Next
+            </button>
         </div>
         
     </div>
